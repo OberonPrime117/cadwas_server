826ef05afcf771fd7bca9e3a69153e104a09ce4d
Aditya Tayade <71881484+OberonPrime117@users.noreply.github.com>
2024-04-03 00:50:28 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update pip
+echo "Updating pip..."
+python3.9 pip install -U pip
+
+# Install dependencies
+
+echo "Installing project dependencies..."
+python3.9 -m pip install -r requirements.txt
