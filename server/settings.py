1f65cef0e33936b793e745b8e60ce30595d631bd
Aditya Tayade <71881484+OberonPrime117@users.noreply.github.com>
2024-04-03 21:31:43 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-$+em&$izbr=@6y*o+kcy#ofli+j*hprn8vbli7jv9*7%g!)2+z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','cadwas-react.vercel.app','server-dev-nzch.2.sg-1.fl0.io']
+ALLOWED_HOSTS = ['127.0.0.1','server-five-henna.vercel.app','cadwas-react.vercel.app','server-dev-nzch.2.sg-1.fl0.io']
 
 
 # Application definition
