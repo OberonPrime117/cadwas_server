ed700735328d36157d5f0ae9ddd12c7d1c14bc97
Aditya Tayade <71881484+OberonPrime117@users.noreply.github.com>
2024-04-03 11:44:32 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-$+em&$izbr=@6y*o+kcy#ofli+j*hprn8vbli7jv9*7%g!)2+z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','https://server-five-henna.vercel.app']
+ALLOWED_HOSTS = ['server-dev-nzch.2.sg-1.fl0.io']
 
 
 # Application definition
